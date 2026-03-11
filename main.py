0fcbcab77a60d52a6237115d4e1eec118c82ce0b
YGY <jasoncoop@126.com>
2026-03-11 17:37:47 +0800

    tccloud commit 2

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 
+import streamlit
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('Home.py')
+def home():
+    return streamlit.html("Home page")
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
